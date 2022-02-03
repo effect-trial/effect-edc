b71a8232f722b57d87a7c2e4025270217d4af957
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-02-03 11:21:14 +0000

    Update AE classification and SAE reason lists to match v0.5 of SAE CRF

--- effect_ae/list_data.py
+++ effect_ae/list_data.py
@@ -2,12 +2,19 @@ from edc_constants.constants import DEAD, NOT_APPLICABLE, OTHER, UNKNOWN
 
 list_data = {
     "edc_adverse_event.aeclassification": [
-        ("lactic_acidosis", "Lactic acidosis"),
-        ("hepatomegaly_steatosis", "Hepatomegaly with steatosis"),
-        ("gastrointestinal_metformin", "Gastrointestinal effects of metformin"),
-        # ("anaemia", "Anaemia"),
-        # ("diarrhoea", "Diarrhoea"),
-        # ("renal_impairment", "Renal impairment"),
+        ("cm", "Cryptococcal Meningitis"),
+        ("anaemia", "Anaemia"),
+        ("bacteraemia/sepsis", "Bacteraemia/Sepsis"),
+        ("CM_IRIS", "CM IRIS"),
+        ("diarrhoea", "Diarrhoea"),
+        ("hypokalaemia", "Hypokalaemia"),
+        ("neutropaenia", "Neutropaenia"),
+        ("pneumonia", "Pneumonia"),
+        ("respiratory_distress", "Respiratory distress"),
+        ("TB", "TB"),
+        ("thrombocytopenia", "Thrombocytopenia"),
+        ("thrombophlebitis", "Thrombophlebitis"),
+        ("renal_impairment", "Renal impairment"),
         (OTHER, "Other"),
     ],
     "edc_adverse_event.saereason": [
@@ -15,13 +22,7 @@ list_data = {
         (DEAD, "Death"),
         ("life_threatening", "Life-threatening"),
         ("significant_disability", "Significant disability"),
-        (
-            "in-patient_hospitalization",
-            (
-                "In-patient hospitalization or prolongation "
-                "(17 or more days from study inclusion)"
-            ),
-        ),
+        ("in-patient_hospitalization", "In-patient hospitalization or prolongation"),
         (
             "medically_important_event",
             "Medically important event (e.g. Severe thrombophlebitis, Bacteraemia, "
