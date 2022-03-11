4ed346425ab06ec611358b13360fdf465eb5ddb4
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-03-11 15:38:46 +0000

    Remove NA option from fluconazole_relation

--- effect_ae/form_validators/ae_initial.py
+++ effect_ae/form_validators/ae_initial.py
@@ -4,15 +4,5 @@ from edc_constants.constants import UNKNOWN, YES
 
 class AeInitialFormValidator(FormValidator):
     def validate_relationship_to_study_drug(self):
-        drugs = [
-            "fluconazole_relation",
-            # TODO: Flucytosine only applicable if a study drug (i.e. on that arm of trial)
-            "flucytosine_relation",
-        ]
-        for drug in drugs:
-            self.applicable_if(
-                YES,
-                UNKNOWN,
-                field="ae_study_relation_possibility",
-                field_applicable=drug,
-            )
+        # TODO: Flucytosine only applicable if a study drug (i.e. on that arm of trial)
+        pass
