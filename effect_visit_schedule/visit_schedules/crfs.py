83c3a936125812ada68b1d181da4bd4012800eea
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-03-18 20:53:52 +0000

    Add SiSx and Mental Status CRFs to each visit, add additional CRFs to d14 visit, update tests

--- effect_visit_schedule/visit_schedules/crfs.py
+++ effect_visit_schedule/visit_schedules/crfs.py
@@ -25,6 +25,7 @@ crfs_prn = FormsCollection(
 
 crfs_unscheduled = FormsCollection(
     Crf(show_order=65, model="effect_subject.adherence"),
+    # TODO: Use correct adherence form for unscheduled visits
     Crf(show_order=70, model="effect_subject.adherencestageone"),
     Crf(show_order=75, model="effect_subject.adherencestagetwo"),
     Crf(show_order=80, model="effect_subject.adherencestagethree"),
@@ -57,36 +58,58 @@ crfs_d01 = FormsCollection(
 
 crfs_d03 = FormsCollection(
     Crf(show_order=10, model="effect_subject.adherencestagetwo"),
+    # TODO: ???Remove neurological symptoms for tel visits
+    Crf(show_order=20, model="effect_subject.signsandsymptoms"),
+    # TODO: ???Remove ECOG/CGS symptoms for tel visits
+    Crf(show_order=40, model="effect_subject.mentalstatus"),
     name=DAY03,
 )
 
 crfs_d09 = FormsCollection(
     Crf(show_order=10, model="effect_subject.adherencestagetwo"),
+    Crf(show_order=20, model="effect_subject.signsandsymptoms"),
+    Crf(show_order=40, model="effect_subject.mentalstatus"),
     name=DAY09,
 )
 
 crfs_d14 = FormsCollection(
     Crf(show_order=10, model="effect_subject.adherencestagethree"),
+    Crf(show_order=20, model="effect_subject.signsandsymptoms"),
+    Crf(show_order=40, model="effect_subject.mentalstatus"),
+    Crf(show_order=80, model="effect_subject.arvtreatment"),
+    Crf(show_order=90, model="effect_subject.patienttreatment"),
+    Crf(show_order=100, model="effect_subject.bloodresultsfbc"),
+    Crf(show_order=400, model="effect_subject.healtheconomics"),
+    Crf(show_order=500, model="effect_subject.clinicalnote"),
     name=DAY14,
 )
 
 crfs_w04 = FormsCollection(
     Crf(show_order=10, model="effect_subject.adherencestagefour"),
+    Crf(show_order=20, model="effect_subject.signsandsymptoms"),
+    Crf(show_order=40, model="effect_subject.mentalstatus"),
     name=WEEK04,
 )
 
 crfs_w10 = FormsCollection(
     Crf(show_order=10, model="effect_subject.adherencestagefour"),
+    Crf(show_order=20, model="effect_subject.signsandsymptoms"),
+    Crf(show_order=40, model="effect_subject.mentalstatus"),
     name=WEEK10,
 )
 
 crfs_w16 = FormsCollection(
     Crf(show_order=10, model="effect_subject.adherencestagefour"),
+    Crf(show_order=20, model="effect_subject.signsandsymptoms"),
+    Crf(show_order=40, model="effect_subject.mentalstatus"),
     name=WEEK16,
 )
 
 crfs_w24 = FormsCollection(
     Crf(show_order=10, model="effect_subject.adherencestagefour"),
+    Crf(show_order=20, model="effect_subject.signsandsymptoms"),
+    Crf(show_order=40, model="effect_subject.mentalstatus"),
     Crf(show_order=400, model="effect_subject.healtheconomics"),
+    # TODO: Termination Form CRF
     name=WEEK24,
 )
