84c2f995f7c78b69d95680ea0a6e236b697ff8c3
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-07-12 16:36:07 +0100

    Remove validation of OTHER_RX_DISCONTINUATION (no longer and offschedule_reason option)

--- effect_prn/form_validators/end_of_study.py
+++ effect_prn/form_validators/end_of_study.py
@@ -7,8 +7,6 @@ from edc_form_validators import FormValidator
 from edc_ltfu.constants import LOST_TO_FOLLOWUP
 from edc_ltfu.modelform_mixins import RequiresLtfuFormValidatorMixin
 
-from ..constants import OTHER_RX_DISCONTINUATION
-
 
 class EndOfStudyFormValidator(
     RequiresDeathReportFormValidatorMixin,
@@ -25,20 +23,11 @@ class EndOfStudyFormValidator(
         self.validate_death_report_if_deceased()
         self.validate_ltfu()
 
-        if self.cleaned_data.get("offschedule_reason"):
-            if self.cleaned_data.get("offschedule_reason").name != OTHER:
-                self.validate_other_specify(
-                    field="offschedule_reason",
-                    other_specify_field="other_offschedule_reason",
-                    other_stored_value=OTHER_RX_DISCONTINUATION,
-                )
-
-            if self.cleaned_data.get("offschedule_reason").name != OTHER_RX_DISCONTINUATION:
-                self.validate_other_specify(
-                    field="offschedule_reason",
-                    other_specify_field="other_offschedule_reason",
-                    other_stored_value=OTHER,
-                )
+        self.validate_other_specify(
+            field="offschedule_reason",
+            other_specify_field="other_offschedule_reason",
+            other_stored_value=OTHER,
+        )
 
         self.required_if(DEAD, field="offschedule_reason", field_required="death_date")
 
