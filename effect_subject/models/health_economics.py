15f240471db6aa5153b9201ebb8314829758fa7e
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-07-20 15:50:17 +0100

    Add validators and help text, update max_lengths, and required status

--- effect_subject/models/health_economics.py
+++ effect_subject/models/health_economics.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from edc_constants.choices import YES_NO
 from edc_model import models as edc_models
@@ -8,16 +9,17 @@ from ..model_mixins import CrfModelMixin
 class HealthEconomics(CrfModelMixin, edc_models.BaseUuidModel):
 
     occupation = models.CharField(
-        verbose_name="What is your occupation/profession?", max_length=45
+        verbose_name="What is your occupation/profession?", max_length=50
     )
 
     education_years = models.IntegerField(
-        verbose_name="How many years of education did you compete?"
+        verbose_name="How many years of education did you compete?",
+        validators=[MinValueValidator(0), MaxValueValidator(30)],
     )
 
     highest_education_certificate = models.CharField(
         verbose_name="What is your highest education certificate?",
-        max_length=45,
+        max_length=50,
     )
 
     primary_school = models.CharField(
@@ -28,6 +30,9 @@ class HealthEconomics(CrfModelMixin, edc_models.BaseUuidModel):
 
     primary_school_years = models.IntegerField(
         verbose_name="If YES, for how many years?",
+        validators=[MinValueValidator(0), MaxValueValidator(12)],
+        null=True,
+        blank=True,
     )
 
     secondary_school = models.CharField(
@@ -38,6 +43,9 @@ class HealthEconomics(CrfModelMixin, edc_models.BaseUuidModel):
 
     secondary_school_years = models.IntegerField(
         verbose_name="If YES, for how many years?",
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
+        null=True,
+        blank=True,
     )
 
     higher_education = models.CharField(
@@ -48,16 +56,21 @@ class HealthEconomics(CrfModelMixin, edc_models.BaseUuidModel):
 
     higher_education_years = models.IntegerField(
         verbose_name="If YES, for how many years?",
+        validators=[MinValueValidator(0), MaxValueValidator(15)],
+        null=True,
+        blank=True,
     )
 
     welfare = models.CharField(
-        verbose_name="Do you received any welfare?",
+        verbose_name="Do you receive any welfare?",
         max_length=15,
         choices=YES_NO,
     )
 
     monthly_household_income = models.IntegerField(
         verbose_name="What is the total income in your household per month?",
+        validators=[MinValueValidator(0)],
+        help_text="In rands or shillings",
     )
 
     highest_income_person = models.CharField(
@@ -69,21 +82,29 @@ class HealthEconomics(CrfModelMixin, edc_models.BaseUuidModel):
     highest_income_person_profession = models.CharField(
         verbose_name="If NO, what is the profession of the person who earns the highest "
         "income?",
-        max_length=45,
+        max_length=50,
+        null=True,
+        blank=True,
     )
 
     monthly_household_food_spent = models.IntegerField(
         verbose_name="How much does your household spend on food in a month?",
+        validators=[MinValueValidator(0)],
+        help_text="In rands or shillings",
     )
 
     monthly_household_rent_spent = models.IntegerField(
         verbose_name="How much does your household spend on rent (or house loan/mortgage) "
         "and utilities in a month?",
+        validators=[MinValueValidator(0)],
+        help_text="In rands or shillings",
     )
 
     yearly_household_large_item_spent = models.IntegerField(
         verbose_name="How much have you spent on large items (e.g., furniture, electrical "
-        "items, cars) in the last year? "
+        "items, cars) in the last year?",
+        validators=[MinValueValidator(0)],
+        help_text="In rands or shillings",
     )
 
     class Meta(CrfModelMixin.Meta, edc_models.BaseUuidModel.Meta):
