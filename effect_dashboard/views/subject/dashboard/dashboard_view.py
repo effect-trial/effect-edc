76bba8f6f1ff08ebf124f7795ec07d62741ebfcd
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-08-19 13:03:39 +0100

    Merge branch 'feat/add-arm-details-subject-dashboard' into develop
    Squashed commit of the following:
    
    commit 9a16a71154b46e55349bad0ba3bfa3ab12838e3a
    Author: Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
    Date:   Fri Aug 19 13:01:58 2022 +0100
    
        Update CHANGES
    
    commit 43a56d3f5f3df349a552df94e00bfe3575e42a4e
    Author: Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
    Date:   Fri Aug 19 12:19:20 2022 +0100
    
        Add arm type and description to Consents sidebar

--- effect_dashboard/views/subject/dashboard/dashboard_view.py
+++ effect_dashboard/views/subject/dashboard/dashboard_view.py
@@ -1,4 +1,15 @@
+from django.apps import apps as django_apps
+from django.core.exceptions import ObjectDoesNotExist
+from edc_constants.constants import CONTROL, INTERVENTION
 from edc_subject_dashboard.views import SubjectDashboardView
+
+
+class InvalidAssignment(Exception):
+    pass
+
+
+class SubjectNotRandomized(Exception):
+    pass
 
 
 class DashboardView(SubjectDashboardView):
@@ -6,3 +17,28 @@ class DashboardView(SubjectDashboardView):
     consent_model = "effect_consent.subjectconsent"
     navbar_selected_item = "consented_subject"
     visit_model = "effect_subject.subjectvisit"
+
+    @property
+    def assignment(self):
+        model_cls = django_apps.get_model("edc_randomization.randomizationlist")
+        subject_identifier = self.kwargs.get("subject_identifier")
+        try:
+            obj = model_cls.objects.get(subject_identifier=subject_identifier)
+        except ObjectDoesNotExist as e:
+            raise SubjectNotRandomized(f"Subject {subject_identifier} not found. Got {e}")
+        return obj.assignment
+
+    def get_context_data(self, **kwargs):
+        if self.assignment == INTERVENTION:
+            assignment_description = "2 weeks fluconazole plus flucytosine"
+        elif self.assignment == CONTROL:
+            assignment_description = "2 weeks fluconazole alone"
+        else:
+            raise InvalidAssignment(f"Invalid assignment.  Got {self.assignment}")
+
+        context = super().get_context_data(**kwargs)
+        context.update(
+            assignment=self.assignment,
+            assignment_description=assignment_description,
+        )
+        return context
