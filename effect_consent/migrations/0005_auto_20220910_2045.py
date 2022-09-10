c74edcc8e688e30ef43fa348313d265c9d27ce01
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-09-10 19:51:54 +0100

    Run migrations through Black

--- effect_consent/migrations/0005_auto_20220910_2045.py
+++ effect_consent/migrations/0005_auto_20220910_2045.py
@@ -6,26 +6,26 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('effect_consent', '0004_auto_20220824_1919'),
+        ("effect_consent", "0004_auto_20220824_1919"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='historicalsubjectreconsent',
-            name='tracking_identifier',
+            model_name="historicalsubjectreconsent",
+            name="tracking_identifier",
         ),
         migrations.RemoveField(
-            model_name='subjectreconsent',
-            name='tracking_identifier',
+            model_name="subjectreconsent",
+            name="tracking_identifier",
         ),
         migrations.AlterField(
-            model_name='historicalsubjectreconsent',
-            name='action_identifier',
+            model_name="historicalsubjectreconsent",
+            name="action_identifier",
             field=models.CharField(blank=True, db_index=True, max_length=50, null=True),
         ),
         migrations.AlterField(
-            model_name='subjectreconsent',
-            name='action_identifier',
+            model_name="subjectreconsent",
+            name="action_identifier",
             field=models.CharField(blank=True, max_length=50, null=True, unique=True),
         ),
     ]
