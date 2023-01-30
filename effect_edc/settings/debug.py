ff64da70701092c4079c0db2ad5111805beedb17
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-01-30 16:37:31 +0000

    Add IDs for all sites to debug.py
    [skip ci]

--- effect_edc/settings/debug.py
+++ effect_edc/settings/debug.py
@@ -6,7 +6,21 @@ from .defaults import *  # noqa
 
 print(f"Settings file {__file__}")  # noqa
 
-SITE_ID = SiteID(default=130)
+# SA Sites
+# SITE_ID = SiteID(default=110)  # Capetown (Khayelitsha & Mitchell’s Plain)
+# SITE_ID = SiteID(default=160)  # Harry Gwala (Pietermaritzburg)
+# SITE_ID = SiteID(default=150)  # King Edward VII
+# SITE_ID = SiteID(default=120)  # Baragwanath
+# SITE_ID = SiteID(default=130)  # Helen Joseph
+# SITE_ID = SiteID(default=140)  # Tshepong (Klerksdorp)
+# SITE_ID = SiteID(default=170)  # Livingstone (Gqeberha)
+SITE_ID = SiteID(default=180)  # Dora Nginza (Gqeberha)
+
+# TZ Sites
+# SITE_ID = SiteID(default=200)  # Amana
+# SITE_ID = SiteID(default=220)  # Mwananyamala
+# SITE_ID = SiteID(default=210)  # Dora Nginza
+
 EDC_SITES_UAT_DOMAIN = False
 DEBUG = True
 ALLOWED_HOSTS = [
