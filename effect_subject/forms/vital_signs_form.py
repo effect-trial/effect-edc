f4721e0890b56fbee4d5a4780b2078f37635635c
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-08-12 10:48:27 +0100

    Restrict field choices for reportable_as_ae and patient_admitted to YES_NO (#418)

--- effect_subject/forms/vital_signs_form.py
+++ effect_subject/forms/vital_signs_form.py
@@ -1,5 +1,6 @@
 from django import forms
 from edc_action_item.forms import ActionItemCrfFormMixin
+from edc_constants.choices import YES_NO
 from edc_crf.modelform_mixins import CrfModelFormMixin
 from effect_form_validators.effect_subject import VitalSignsFormValidator
 
@@ -8,6 +9,11 @@ from ..models import VitalSigns
 
 class VitalSignsForm(CrfModelFormMixin, ActionItemCrfFormMixin, forms.ModelForm):
     form_validator_cls = VitalSignsFormValidator
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["reportable_as_ae"].choices = YES_NO
+        self.fields["patient_admitted"].choices = YES_NO
 
     class Meta(ActionItemCrfFormMixin.Meta):
         model = VitalSigns
