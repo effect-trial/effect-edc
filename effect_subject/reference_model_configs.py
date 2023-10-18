76f65a85ae3c483a33a9e78d89bec08d9032c33e
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-10-18 13:50:48 +0100

    Fix server errors on saving proxy models (#711)
    Added reference model configs for Study Medication and Adherence models,
     that are only accessed via their proxy counterparts.

--- effect_subject/reference_model_configs.py
+++ effect_subject/reference_model_configs.py
@@ -1,7 +1,23 @@
-from edc_reference import site_reference_configs
+from edc_reference import ReferenceModelConfig, site_reference_configs
 
 site_reference_configs.register_from_visit_schedule(
     visit_models={"edc_appointment.appointment": "effect_subject.subjectvisit"}
+)
+
+# Register main Adherence model (only accessed via proxy models)
+site_reference_configs.register(
+    reference_config=ReferenceModelConfig(
+        "effect_subject.adherence",
+        fields=["report_datetime"],
+    )
+)
+
+# Register main Study Medication model (only accessed via proxy models)
+site_reference_configs.register(
+    reference_config=ReferenceModelConfig(
+        "effect_subject.studymedication",
+        fields=["report_datetime"],
+    )
 )
 
 site_reference_configs.add_fields_to_config(
