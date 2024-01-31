938a438f3ea455f5f18cfe63c9caa8092f8f6a43
erikvw <ew2789@gmail.com>
2024-01-30 22:42:43 -0600

    refactor screening_listboard buttons

--- effect_dashboard/templatetags/effect_dashboard_extras.py
+++ effect_dashboard/templatetags/effect_dashboard_extras.py
@@ -5,9 +5,8 @@ from typing import TYPE_CHECKING
 from bs4 import BeautifulSoup
 from django import template
 from django.conf import settings
-from django.contrib.auth import get_permission_codename
 from django.core.exceptions import ObjectDoesNotExist
-from edc_constants.constants import INCOMPLETE, NO, PENDING, TBD, YES
+from edc_constants.constants import INCOMPLETE, PENDING, TBD
 from edc_dashboard.url_names import url_names
 from edc_refusal.models import SubjectRefusal
 from edc_subject_dashboard.view_utils import NextQuerystring
@@ -20,41 +19,6 @@ if TYPE_CHECKING:
 
 
 register = template.Library()
-
-
-@register.inclusion_tag(
-    f"effect_dashboard/bootstrap{settings.EDC_BOOTSTRAP}/" f"buttons/screening_button.html",
-    takes_context=True,
-)
-def render_screening_button(context, subject_screening: SubjectScreening):
-    nq = NextQuerystring(
-        next_url_name="screening_listboard_url",
-        reverse_kwargs=dict(
-            screening_identifier=subject_screening.screening_identifier,
-        ),
-        extra_kwargs=dict(
-            screening_identifier=subject_screening.screening_identifier,
-            gender=subject_screening.gender,
-            initials=subject_screening.initials,
-            modified=subject_screening.modified,
-        ),
-    )
-    url = f"{subject_screening.get_absolute_url()}?{nq.querystring}"
-    fa_icon = "fa-eye" if subject_screening.consented else "fa-pencil"
-    title = ("View" if subject_screening.consented else "Edit") + " subject's screening form"
-
-    return dict(
-        perms=context["perms"],
-        screening_identifier=subject_screening.screening_identifier,
-        eligible=subject_screening.eligible,
-        href=url,
-        fa_icon=fa_icon,
-        title=title,
-        NO=NO,
-        PENDING=PENDING,
-        TBD=TBD,
-        YES=YES,
-    )
 
 
 @register.inclusion_tag(
@@ -78,50 +42,6 @@ def render_eligibility_button(subject_screening: SubjectScreening):
         tooltip=tooltip,
         TBD=TBD,
         btn_color=btn_color,
-    )
-
-
-@register.inclusion_tag(
-    f"effect_dashboard/bootstrap{settings.EDC_BOOTSTRAP}/buttons/consent_button.html",
-    takes_context=True,
-)
-def render_consent_button(context, subject_screening: SubjectScreening):
-    nq = NextQuerystring(
-        next_url_name="screening_listboard_url",
-        reverse_kwargs=dict(
-            screening_identifier=subject_screening.screening_identifier,
-        ),
-        extra_kwargs=dict(
-            screening_identifier=subject_screening.screening_identifier,
-            gender=subject_screening.gender,
-            initials=subject_screening.initials,
-            modified=subject_screening.modified,
-        ),
-    )
-    model_cls = subject_screening.consent_definition.model_cls
-    subject_consent = None
-    try:
-        subject_consent = model_cls.objects.get(
-            screening_identifier=subject_screening.screening_identifier
-        )
-    except ObjectDoesNotExist:
-        url = f"{model_cls().get_absolute_url()}?{nq.querystring}"
-    else:
-        url = f"{subject_consent.get_absolute_url()}?{nq.querystring}"
-    fa_icon = "fa-pencil" if subject_consent else "fa-plus"
-    title = "Edit consent" if subject_consent else "Consent subject to participate."
-    add_codename = get_permission_codename("add", model_cls._meta)
-    change_codename = get_permission_codename("change", model_cls._meta)
-    codenames = [f"effect_consent.{add_codename}", f"effect_consent.{change_codename}"]
-    disabled = "" if context["request"].user.has_perms(codenames) else "disabled"
-    return dict(
-        disabled=disabled,
-        perms=context["perms"],
-        screening_identifier=subject_screening.screening_identifier,
-        href=url,
-        consent_version=subject_screening.consent_definition.version,
-        title=title,
-        fa_icon=fa_icon,
     )
 
 
--- effect_screening/admin/subject_screening_admin.py
+++ effect_screening/admin/subject_screening_admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-from django.template.loader import render_to_string
-from django.urls.base import reverse
-from django.urls.exceptions import NoReverseMatch
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils.html import conditional_escape, format_html
-from django.utils.translation import gettext as _
 from django_audit_fields.admin import audit_fieldset_tuple
-from edc_dashboard.url_names import url_names
 from edc_model_admin.dashboard import ModelAdminSubjectDashboardMixin
 from edc_model_admin.history import SimpleHistoryAdmin
+from edc_model_admin.mixins import ModelAdminHideDeleteButtonOnCondition
 from edc_sites.admin import SiteModelAdminMixin
 
 from effect_screening.eligibility import ScreeningEligibility
@@ -19,13 +16,14 @@ from ..models import SubjectScreening
 
 @admin.register(SubjectScreening, site=effect_screening_admin)
 class SubjectScreeningAdmin(
-    ModelAdminSubjectDashboardMixin, SiteModelAdminMixin, SimpleHistoryAdmin
+    ModelAdminSubjectDashboardMixin,
+    ModelAdminHideDeleteButtonOnCondition,
+    SiteModelAdminMixin,
+    SimpleHistoryAdmin,
 ):
     form = SubjectScreeningForm
 
     post_url_on_delete_name = "screening_listboard_url"
-    subject_listboard_url_name = "screening_listboard_url"
-    subject_dashboard_url_name = "screening_listboard_url"
 
     additional_instructions = (
         "Patients must meet ALL of the inclusion criteria and NONE of the "
@@ -253,19 +251,9 @@ class SubjectScreeningAdmin(
         eligibility = ScreeningEligibility(obj)
         return format_html(conditional_escape(eligibility.display_label))
 
-    def dashboard(self, obj=None, label=None):
+    def hide_delete_button_on_condition(self, request, object_id) -> bool:
         try:
-            url = reverse(
-                self.get_subject_dashboard_url_name(),
-                kwargs=self.get_subject_dashboard_url_kwargs(obj),
-            )
-        except NoReverseMatch:
-            url = reverse(url_names.get("screening_listboard_url"), kwargs={})
-            context = dict(
-                title=_("Go to screening listboard"),
-                url=f"{url}?q={obj.screening_identifier}",
-                label=label,
-            )
-        else:
-            context = dict(title=_("Go to subject dashboard"), url=url, label=label)
-        return render_to_string("dashboard_button.html", context=context)
+            obj = SubjectScreening.objects.get(id=object_id)
+        except ObjectDoesNotExist:
+            return False
+        return obj.consented
