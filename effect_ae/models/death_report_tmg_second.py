405b30f09577fbb4495c99fd3eba923ff3864562
erikvw <ew2789@gmail.com>
2024-01-31 20:32:41 -0600

    fix death report tmg second

--- effect_ae/models/death_report_tmg_second.py
+++ effect_ae/models/death_report_tmg_second.py
@@ -1,8 +1,19 @@
-from edc_adverse_event.model_mixins import DeathReportTmgSecondModelMixin
+from edc_adverse_event.constants import DEATH_REPORT_TMG_SECOND_ACTION
+from edc_adverse_event.model_mixins import (
+    DeathReportTmgSecondManager,
+    DeathReportTmgSecondModelMixin,
+    DeathReportTmgSecondSiteManager,
+)
 
 from effect_ae.models import DeathReportTmg
 
 
 class DeathReportTmgSecond(DeathReportTmgSecondModelMixin, DeathReportTmg):
+    action_name = DEATH_REPORT_TMG_SECOND_ACTION
+
+    objects = DeathReportTmgSecondManager()
+
+    on_site = DeathReportTmgSecondSiteManager()
+
     class Meta(DeathReportTmgSecondModelMixin.Meta):
         proxy = True
