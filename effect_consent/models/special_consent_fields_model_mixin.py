162d64e442e9cc8cab334277d8c475d243445832
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2024-03-01 12:57:41 +0000

    Use 'participant' not 'ppt' for consistency

--- effect_consent/models/special_consent_fields_model_mixin.py
+++ effect_consent/models/special_consent_fields_model_mixin.py
@@ -6,27 +6,29 @@ class SpecialConsentFieldsModelMixin(models.Model):
     """These fields were added around 1 March 2024"""
 
     he_substudy = models.CharField(
-        verbose_name="Does the ppt agree to participate in the Health Economics sub-study?",
+        verbose_name=(
+            "Does the participant agree to participate in the Health Economics sub-study?"
+        ),
         max_length=15,
         choices=YES_NO,
     )
 
     sample_storage = models.CharField(
-        verbose_name="Does the ppt agree to storage of samples in-country?",
+        verbose_name="Does the participant agree to storage of samples in-country?",
         max_length=15,
         choices=YES_NO,
     )
 
     sample_export = models.CharField(
-        verbose_name="Does the ppt agree to export of stored samples internationally?",
+        verbose_name="Does the participant agree to export of stored samples internationally?",
         max_length=15,
         choices=YES_NO,
     )
 
     hcw_data_sharing = models.CharField(
         verbose_name=(
-            "Does the ppt agree to sharing study information with "
-            "their primary healthcare provider? "
+            "Does the participant agree to sharing study information with "
+            "their primary healthcare provider?"
         ),
         max_length=15,
         choices=YES_NO,
