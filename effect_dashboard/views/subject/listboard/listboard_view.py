34f38de6c7918d8b9178cf6a66a8892cfaba579f
erikvw <ew2789@gmail.com>
2024-04-24 10:55:54 -0500

    annotate with Min consent_datetime

--- effect_dashboard/views/subject/listboard/listboard_view.py
+++ effect_dashboard/views/subject/listboard/listboard_view.py
@@ -1,4 +1,4 @@
-from django.db.models import Max, Q
+from django.db.models import Min, Q
 from edc_dashboard.view_mixins import EdcViewMixin
 from edc_listboard.view_mixins import ListboardFilterViewMixin, SearchFormViewMixin
 from edc_listboard.views import ListboardView as BaseListboardView
@@ -32,7 +32,7 @@ class ListboardView(
     def get_updated_queryset(self, queryset):
         """Only return records with latest consent for each subject."""
         sub_qs = queryset.values("subject_identifier").annotate(
-            max_consent_datetime=Max("consent_datetime")
+            max_consent_datetime=Min("consent_datetime")
         )
         queryset = queryset.filter(
             subject_identifier__in=sub_qs.values("subject_identifier"),
