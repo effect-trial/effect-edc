b7b03cc1e1ab83f04c146dab0baeaa8dbe050e2a
erikvw <ew2789@gmail.com>
2024-09-10 09:26:09 -0500

    update settings.STORAGES

--- effect_edc/settings/defaults.py
+++ effect_edc/settings/defaults.py
@@ -482,7 +482,7 @@ if env("AWS_ENABLED"):
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_LOCATION = env.str("AWS_LOCATION")
     AWS_IS_GZIPPED = True
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    STORAGES = {"staticfiles": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
     STATIC_URL = f"{os.path.join(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)}/"
     STATIC_ROOT = ""
 else:
