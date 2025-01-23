568ad7fcf409733c959c735d0d7cc05fa70d8380
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2025-01-23 11:08:34 +0000

    Reset default debug site

--- effect_edc/settings/debug.py
+++ effect_edc/settings/debug.py
@@ -7,11 +7,11 @@ from .defaults import *  # noqa
 print(f"Settings file {__file__}")  # noqa
 
 # SA Sites
-# SITE_ID = SiteID(default=110)  # Capetown (Khayelitsha & Mitchell’s Plain)
+SITE_ID = SiteID(default=110)  # Capetown (Khayelitsha & Mitchell’s Plain)
 # SITE_ID = SiteID(default=160)  # Harry Gwala (Pietermaritzburg)
 # SITE_ID = SiteID(default=150)  # King Edward VII
 # SITE_ID = SiteID(default=120)  # Baragwanath
-SITE_ID = SiteID(default=130)  # Helen Joseph
+# SITE_ID = SiteID(default=130)  # Helen Joseph
 # SITE_ID = SiteID(default=140)  # Tshepong (Klerksdorp)
 # SITE_ID = SiteID(default=170)  # Livingstone (Gqeberha)
 # SITE_ID = SiteID(default=180)  # Dora Nginza (Gqeberha)
