fdcb9684e53b74b0ffab077b1a3ffe6b5a0c8573
erikvw <ew2789@gmail.com>
2025-09-09 23:37:40 -0500

    lint

--- runtests.py
+++ runtests.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
+
 from edc_test_settings.func_main import func_main2
 
 if __name__ == "__main__":
 
-    tests = [
-        # "tests",
-        # "effect_ae.tests",
-        # "effect_dashboard.tests",
-        # "effect_edc.tests",
-        # "effect_labs.tests",
-        # "effect_lists.tests",
-        # "effect_prn.tests",
-        # "effect_screening.tests",
-        # "effect_subject.tests",
-    ]
-    func_main2("tests.test_settings")
+    func_main2(
+        "tests.test_settings",
+        "effect_ae.tests",
+        "effect_dashboard.tests",
+        "effect_edc.tests",
+        "effect_labs.tests",
+        "effect_lists.tests",
+        "effect_prn.tests",
+        "effect_screening.tests",
+        "effect_subject.tests",
+    )
