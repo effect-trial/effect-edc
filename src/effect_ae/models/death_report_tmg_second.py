242b9053f084e342e43085e79b7bec2ad76c2ec4
erikvw <ew2789@gmail.com>
2026-04-22 14:33:31 -0500

    management command to import TMG2 data from excel

--- src/effect_ae/models/death_report_tmg_second.py
+++ src/effect_ae/models/death_report_tmg_second.py
@@ -1,19 +1,31 @@
+from clinicedc_constants import NOT_EVALUATED, NULL_STRING
+from django.db import models
+from edc_action_item.managers import ActionIdentifierModelManager, ActionIdentifierSiteManager
 from edc_adverse_event.constants import DEATH_REPORT_TMG_SECOND_ACTION
-from edc_adverse_event.model_mixins import (
-    DeathReportTmgSecondManager,
-    DeathReportTmgSecondModelMixin,
-    DeathReportTmgSecondSiteManager,
-)
+from edc_adverse_event.model_mixins import DeathReportTmgModelMixin
+from edc_model.models import BaseUuidModel
 
-from ..models import DeathReportTmg
+from ..choices import CRYPTOCOCCAL_RELATIONSHIP
 
 
-class DeathReportTmgSecond(DeathReportTmgSecondModelMixin, DeathReportTmg):
+class DeathReportTmgSecond(DeathReportTmgModelMixin, BaseUuidModel):
     action_name = DEATH_REPORT_TMG_SECOND_ACTION
 
-    objects = DeathReportTmgSecondManager()
+    cryptococcal_relatedness = models.CharField(
+        verbose_name="In your opinion, is the cause death related to cryptococcal infection?",
+        max_length=25,
+        choices=CRYPTOCOCCAL_RELATIONSHIP,
+        default=NOT_EVALUATED,
+    )
 
-    on_site = DeathReportTmgSecondSiteManager()
+    cryptococcal_relatedness_comment = models.TextField(
+        max_length=250, default=NULL_STRING, blank=True
+    )
 
-    class Meta(DeathReportTmgSecondModelMixin.Meta):
-        proxy = True
+    objects = ActionIdentifierModelManager()
+
+    on_site = ActionIdentifierSiteManager()
+
+    class Meta(DeathReportTmgModelMixin.Meta, BaseUuidModel.Meta):
+        verbose_name = "Death Report TMG (2nd)"
+        verbose_name_plural = "Death Report TMG (2nd)"
